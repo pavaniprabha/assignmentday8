206eb85162130f0e3733d45bfc1f560c354d6554
RAGOLUpavani <ragolupavani0330@gmail.com>
2021-08-07 17:59:19 +0530

    encapsulated :input program is added

---
+++ src/com/blz/main/dto/Inputs.java
@@ -0,0 +1,32 @@
+package com.blz.main.dto;
+
+public class Inputs {
+	private static double x1;
+    private static double x2;
+    private static double y1;
+    private static double y2;
+	public static double getX1() {
+		return x1;
+	}
+	public static void setX1(double x1) {
+		Inputs.x1 = x1;
+	}
+	public static double getX2() {
+		return x2;
+	}
+	public static void setX2(double x2) {
+		Inputs.x2 = x2;
+	}
+	public static double getY1() {
+		return y1;
+	}
+	public static void setY1(double y1) {
+		Inputs.y1 = y1;
+	}
+	public static double getY2() {
+		return y2;
+	}
+	public static void setY2(double y2) {
+		Inputs.y2 = y2;
+	}
+}
