d77fd688fa5a28a7c6243ed7249fe39d27a807eb
RAGOLUpavani <ragolupavani0330@gmail.com>
2021-08-07 18:03:04 +0530

    utility program is added

---
+++ src/com/blz/main/services/uitility/UserUtility.java
@@ -0,0 +1,10 @@
+package com.blz.main.services.uitility;
+
+import java.util.Scanner;
+
+public class UserUtility {
+	  public static Scanner SC= new Scanner(System.in);
+	    public static int intInput(){
+	        return SC.nextInt();
+	    }
+}
