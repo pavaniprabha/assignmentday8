0686fbefd1872ade0d03052b405c3bb710197d5d
RAGOLUpavani <ragolupavani0330@gmail.com>
2021-08-09 09:26:35 +0530

    inputs are executed and added

---
+++ src/com/blz/main/dto/Inputs.java
@@ -0,0 +1,34 @@
+package com.blz.main.dto;
+
+public class Inputs {
+	    private static int x1;
+	    private static int x2;
+	    private static int y1;
+	    private static int y2;
+		public static int getX1() {
+			return x1;
+		}
+		public static void setX1(int x1) {
+			Inputs.x1 = x1;
+		}
+		public static int getX2() {
+			return x2;
+		}
+		public static void setX2(int x2) {
+			Inputs.x2 = x2;
+		}
+		public static int getY1() {
+			return y1;
+		}
+		public static void setY1(int y1) {
+			Inputs.y1 = y1;
+		}
+		public static int getY2() {
+			return y2;
+		}
+		public static void setY2(int y2) {
+			Inputs.y2 = y2;
+		}
+	
+
+}
