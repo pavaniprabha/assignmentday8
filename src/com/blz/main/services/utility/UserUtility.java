8a7359c7abe06f8f0286d5305907cbd9c193f31e
RAGOLUpavani <ragolupavani0330@gmail.com>
2021-08-09 09:30:26 +0530

    utility progarm is added

---
+++ src/com/blz/main/services/utility/UserUtility.java
@@ -0,0 +1,11 @@
+package com.blz.main.services.utility;
+
+import java.util.Scanner;
+
+public class UserUtility {
+	  public static Scanner SC= new Scanner(System.in);
+	    public static int intInput(){
+	        return SC.nextInt();
+	    }
+
+}
